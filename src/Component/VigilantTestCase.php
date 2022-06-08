45cef194498765823348a4ba6d2b70e44a58b0d4
ivpel <ivan.pelykh@protonmail.com>
2022-06-08 21:03:37 +0300

    Added VigilantTestCase as entry point for functional tests

---
+++ src/Component/VigilantTestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Vigilant\Component;
+
+use PHPUnit\Framework\TestCase;
+use Vigilant\Traits\InteractionTrait;
+
+class VigilantTestCase extends TestCase
+{
+    use InteractionTrait;
+}
